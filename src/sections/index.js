acf9a1556695865075510c57d1081417eb6084bc
sanketjs <sanket.kere28@gmail.com>
2024-03-18 16:49:35 +0530

    nike

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import PopularProducts from "./PopularProducts";
+import Hero from "./Hero";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+    Hero,
+    Nav,
+    Footer,
+    SpecialOffer,
+    Subscribe,
+    CustomerReviews,
+    PopularProducts,
+    SuperQuality,
+    Services
+}
